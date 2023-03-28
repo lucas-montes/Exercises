f942e9dd8c19a29762b74ec1d83a095f41264c82
Lucas <lluc23@hotmail.com>
2023-03-28 20:06:17 +0200

    sudoku

---
+++ leetcode/ArraysAndHashing/isValidSudoku.py
@@ -0,0 +1,89 @@
+import itertools
+import sys
+from collections import defaultdict
+from typing import List
+
+sys.tracebacklimit = 0
+
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        cols = defaultdict(set)
+        rows = defaultdict(set)
+        squares = defaultdict(set)
+
+        for c, r in itertools.product(range(9), range(9)):
+            number = board[r][c]
+            if number == ".":
+                continue
+            if (
+                number in cols[c]
+                or number in rows[r]
+                or number in squares[(r // 3, c // 3)]
+            ):
+                return False
+            cols[c].add(number)
+            rows[r].add(number)
+            squares[(r // 3, c // 3)].add(number)
+        return True
+
+
+def run_test(expected, test_data, method):
+    result = getattr(Solution(), method)(*test_data)
+    assert (
+        expected == result
+    ), f"at test with params {test_data} result is {result} and the expected value: {expected}"
+
+
+run_test(
+    True,
+    [
+        [
+            ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+            ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+            [".", "9", "8", ".", ".", ".", ".", "6", "."],
+            ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+            ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+            ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+            [".", "6", ".", ".", ".", ".", "2", "8", "."],
+            [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+            [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+        ]
+    ],
+    "isValidSudoku",
+)
+run_test(
+    False,
+    [
+        [
+            ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+            ["6", "8", ".", "1", "9", "5", ".", ".", "."],
+            [".", "9", "8", ".", ".", ".", ".", "6", "."],
+            ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+            ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+            ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+            [".", "6", ".", ".", ".", ".", "2", "8", "."],
+            [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+            [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+        ]
+    ],
+    "isValidSudoku",
+)
+run_test(
+    False,
+    [
+        [
+            ["8", "3", ".", ".", "7", ".", ".", ".", "."],
+            ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+            [".", "9", "8", ".", ".", ".", ".", "6", "."],
+            ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+            ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+            ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+            [".", "6", ".", ".", ".", ".", "2", "8", "."],
+            [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+            [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+        ]
+    ],
+    "isValidSudoku",
+)
+# run_test(RESULT, [ARGS], 'isValidSudoku')
