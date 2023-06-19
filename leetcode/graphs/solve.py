d27e2f0099c8095e0e58a360c257af6e19948ca3
Lucas <lluc23@hotmail.com>
2023-06-19 19:11:52 +0200

    2023_06_19T19_11_49

--- leetcode/graphs/solve.py
+++ leetcode/graphs/solve.py
@@ -7,45 +7,32 @@ sys.tracebacklimit = 0
 
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
-        rows, cols = range(len(board)), range(len(board[0]))
+        directions = [
+            [0, 1],
+            [0, -1],
+            [1, 0],
+            [-1, 0],
+        ]
+        rows, cols = len(board), len(board[0])
         seen = set()
-        is_x = set()
-        is_o = set()
 
-        def dfs(r, c):
-            q = deque()
-            seen.add((r, c))
-            q.append((r, c))
-            while q:
-                row, col = q.popleft()
-                around = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-                sr = 0
-                for dr, dc in around:
-                    r, c = row + dr, col + dc
-                    if r in rows and c in cols:
-                        if (r, c) not in seen:
-                            if board[r][c] == "X":
-                                sr += 1
-                                is_x.add((r, c))
-                            else:
-                                is_o.add((r, c))
-                                q.append((r, c))
-                            seen.add((r, c))
-                        else:
-                            if (r, c) in is_x:
-                                sr += 1
-                if sr == 4:
-                    board[r][c] = "X"
+        queue = deque()
+        queue.append((0, 0))
+        while queue:
+            curr_row, curr_col = queue.popleft()
 
-        for r in rows:
-            for c in cols:
-                if board[r][c] == "O" and (r, c) not in seen:
-                    is_o.add((r, c))
-                    dfs(r, c)
-                else:
-                    seen.add((r, c))
-                    is_x.add((r, c))
-        return board
+            for dx, dy in directions:
+                new_row = curr_row + dx
+                new_col = curr_col + dy
+
+                if (
+                    0 <= new_row < rows
+                    and 0 <= new_col < cols
+                    and (new_row, new_col) not in seen
+                    and board[new_row][new_col] == 0
+                ):
+                    seen.add((new_row, new_col))
+                    queue.append((new_row, new_col))
 
 
 def run_test(method, expected, *args, **kwargs):
